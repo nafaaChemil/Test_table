c65bef4d2ffe6f766af1da781951b6fd2cdcb83b
NafaaCHEMIL <nafaa.chemil@gmail.com>
2021-05-29 15:47:41 +0200

    clean

--- src/components/table3ApiAxios.js
+++ src/components/table3ApiAxios.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory, {
 	PaginationProvider,
